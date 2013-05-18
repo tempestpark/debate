1f2cd8a7ba879ac0221a903cefe57e5779f3453e
dave911d <davidpatch98@gmail.com>
2013-05-18 13:11:52 +1000

    moving on with the basic schema with fields etc

---
+++ models/index.js
@@ -0,0 +1,17 @@
+module.exports = function(mongoose) {
+	// Debate topic Schema 
+var debateSchema = mongoose.Schema({
+  name: { type: String, required: true, unique: true },
+  username: { type: String, required: true, unique: true },
+  for_against: { type: Boolean , required: true, unique: false },
+  createdate: {type: Date, default: Date.now,  required: true},
+  author: { type: String, required: true, unique: true },
+  replies: [commentSchema]
+});
+}
+
+var commentSchema = mongoose.Schema({
+
+
+});
+}
